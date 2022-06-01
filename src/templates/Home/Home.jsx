4e1599cb378c66e63f011319a238613fad5a7364
Lucas Lima <48462708+limalucas99@users.noreply.github.com>
2022-06-01 06:47:42 -0300

    useImperativeHandle +forwardRef

--- src/templates/Home/Home.jsx
+++ src/templates/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { forwardRef, useEffect, useImperativeHandle, useLayoutEffect, useRef, useState } from "react";
 
 export const Home = () => {
 
@@ -9,24 +9,45 @@ export const Home = () => {
   useLayoutEffect(() => { // FAZ O DOM ESPERAR PARA QUE OS ELEMENTOS SEJAM MODIFICADOS SIMULTÂNEAMENTE
     const now = Date.now() // useLayoutEffect - SÓ ATUALIZA OS ELEMENTOS JUNTOS DPS QUE A EXECUÇÃO TERMINA
     while (Date.now() < now + 600)
-    divRef.current.scrollTop = divRef.current.scrollHeight;
+    divRef.current.divRef.scrollTop = divRef.current.divRef.scrollHeight;
   })
 
   const handleClick = () => {
     setCounted(c => [...c, +c.slice(-1) + 1])
+    divRef.current.handleClick()
   }
 
   return (
     <>
     <button onClick={handleClick}>Count {counted.slice(-1)}</button>
-    <div ref={divRef} style={{height: '100px', width: '100px', overflowY: 'scroll'}}>
-    {counted.map(c => {
-      return <p key={`c-${c}`}>{c}</p>
-    })}
-    </div> 
+    <DisplayCounted counted={counted} ref={divRef}/>
     </>
   )
 
 }
+
+export const DisplayCounted = forwardRef(function ({counted}, ref) { // forwardRef - É utilizado para passar ref de um componente para o outro
+// Recebe um componente como parâmetro
+
+  const [rand, setRand] = useState('0.24')
+  const divRef = useRef()
+
+  const handleClick = () => {
+    setRand(Math.random().toFixed(2));
+  }
+
+  useImperativeHandle(ref, () => ({
+    handleClick,
+    divRef: divRef.current
+  }))
+
+  return (
+        <div ref={divRef} style={{height: '100px', width: '100px', overflowY: 'scroll'}}>
+    {counted.map(c => {
+      return <p onClick={handleClick} key={`c-${c}`}>{c} +++ {rand}</p>
+    })}
+    </div> 
+  )
+})
 
 export default Home;
